ae556f38cd8969033317bc0b4fd3ea95c8a5285f
MattNewZ01 <matthewchu1998@gmail.com>
2022-10-10 10:15:31 +0800

    .

--- public/js/forms.js
+++ public/js/forms.js
@@ -3,57 +3,63 @@ function loginForm() {
     accountContainer.innerHTML = `
     <div class="header">Welcome</div>
     <form id="loginForm">
-    <label>
-        <input type="text" name="email" placeholder="Email" id="reg-email" />
-    </label>
-    <label>
-        <input type="password" name="password" placeholder="Password" id="reg-password" />
-    </label>
- 
-    <div class="google-button">
-    <a href="/connect/google" class="btn btn-success"><i class="fa-brands fa-google"></i></a>
-    </div>
-   
-    <input type="submit" value="Login" class="btn" id="login-button">
-    
-  
-    <div>
-        Not a member?
-    </div>
-    <div class="btn" id="login" onclick="signupForm()">Signup</div>
-    </form>
+
+
+                <div class="field">
+                    <input type="text" required autocomplete="off" id="reg-email">
+                    <label for="reg-email" title="Email" data-title="Email"></label>
+                </div>
+                <div class="field">
+                    <input type="password" required autocomplete="off" id="reg-password">
+                    <label for="reg-password" title="Password" data-title="Password"></label>
+                </div>
+
+                <div class="google-button">
+                    <a href="/connect/google" class="btn btn-success"><i class="fa-brands fa-google"></i></a>
+                </div>
+                <br>
+                <input type="submit" value="Login" class="btn" id="login-button">
+
+                <br>
+                <div>
+                    Not a member?
+                </div>
+                <br>
+                <div class="" id="login" onclick="signupForm()">Signup</div>
+            </form>
  `
 }
 function signupForm() {
     accountContainer.innerHTML = `
     <div class="header">Welcome</div>
     <form id="signupForm">
-    <label>
-        <input type="text" name="email" placeholder="Email" id="reg-email" />
-    </label>
-    <label>
-        <input type="text" name="username" placeholder="Username" id="reg-username" />
-    </label>
-    <label>
-        <input type="password" name="password" placeholder="Password" id="reg-password" />
-    </label>
-    <!-- <label>
-        Profile Picture:
-        <input type="file" value="Choose File" name="UserImage" placeholder="file" class="button"
-            id="userImage" />
-    </label> -->
+    <div class="field">
+    <input type="text" required autocomplete="off" id="reg-username">
+    <label for="reg-username" title="Username" data-title="Username"></label>
+</div>
+    <div class="field">
+    <input type="text" required autocomplete="off" id="reg-email">
+    <label for="reg-email" title="Email" data-title="Email"></label>
+</div>
+
+<div class="field">
+    <input type="password" required autocomplete="off" id="reg-password">
+    <label for="reg-password" title="Password" data-title="Password"></label>
+</div>
+
   
         <div class="google-button">
             <a href="/connect/google" class="btn btn-success"><i class="fa-brands fa-google"></i></a>
         </div>
-      
+      <br>
             <input type="submit" value="Sign Up" class="btn" id="signup-button">
         
-   
+   <br>
     <div>
         Already a member?
     </div>
-    <div class="btn" id="login" onclick="loginForm()">Login</div>
+    <br>
+    <div class="" id="login" onclick="loginForm()">Login</div>
 </form>
     `
 }
