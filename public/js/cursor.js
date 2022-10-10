171663d3b985ae3e96bb2c0a103f3a2717cf0334
MattNewZ01 <matthewchu1998@gmail.com>
2022-10-10 11:16:52 +0800

    Update cursor

--- public/js/cursor.js
+++ public/js/cursor.js
@@ -1,19 +1,31 @@
-var cursor = document.getElementById("cursor");
+function update(e) {
+  var x = e.clientX || e.touches[0].clientX
+  var y = e.clientY || e.touches[0].clientY
+  const windowX = window.innerWidth || document.documentElement.clientWidth ||
+    document.body.clientWidth;
+  const windowY = window.innerHeight || document.documentElement.clientHeight ||
+    document.body.clientHeight;
 
-document.body.addEventListener("mousemove", function (e) {
-    cursor.style.left = e.clientX + "px",
-        cursor.style.top = e.clientY + "px";
+  document.documentElement.style.setProperty('--cursorX', x + 'px')
+  document.documentElement.style.setProperty('--cursorY', y + 'px')
+  document.documentElement.style.setProperty('--cursorNegX', -x + 'px')
+  document.documentElement.style.setProperty('--cursorNegY', -y + 'px')
+  document.documentElement.style.setProperty('--X', windowX + 200 + 'px')
+  document.documentElement.style.setProperty('--Y', windowY + 200 + 'px')
+}
 
-});
-
-var background = document.querySelector('.background');
+document.addEventListener('mousemove', update)
+document.addEventListener('touchmove', update)
+var background = document.querySelector('.background-mask');
+var cursor = document.querySelector('.cursor');
 let x = 2
 function changeBackround() {
-    if (x > 3) {
-        x = 1
-    }
-    background.style.backgroundImage = `url('./img/background${x}.jpeg')`
-    x++
+  if (x > 3) {
+    x = 1
+  }
+  background.style.backgroundImage = `url('./img/background${x}.jpeg')`
+  cursor.style.backgroundImage = `url('./img/background${x}.jpeg')`
+  x++
 
 }
 
