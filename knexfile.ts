661f4ef8836b89615c420135fff3f88c04d1d1ed
MattNewZ01 <matthewchu1998@gmail.com>
2022-10-12 14:55:03 +0800

    add main.yml

--- knexfile.ts
+++ knexfile.ts
@@ -36,7 +36,22 @@ const config: { [key: string]: Knex.Config } = {
       tableName: "knex_migrations"
     }
   },
-
+  test: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
   production: {
     client: "postgresql",
     connection: {
